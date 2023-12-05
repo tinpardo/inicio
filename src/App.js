abd90a16b87630c7b36f024d3fd1ab882d6e57d2
tinpardo@gmail.com <tinpardo@gmail.com>
2023-12-05 09:51:20 -0500

    Segunda borrada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <h1>hola mundo</h1>
     </div>
   );
 }
